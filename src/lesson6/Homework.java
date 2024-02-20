1d6fd164b88542cf5c1f96b79a3adad152a7759c
maddojka <yuriy.soroko@gmail.com>
2024-02-20 22:04:18 +0300

    домашнее задание 6 занятие

--- src/lesson6/Homework.java
+++ src/lesson6/Homework.java
@@ -63,5 +63,32 @@ public class Homework {
             }
         }
         System.out.println(maxSum);
+
+        // Two sum O(n^2)
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+        // Unique values first
+        int[] numbers = new int[]{1, 0, 9, 0, 5, 1, 4, 3, 2, 2, 8};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
