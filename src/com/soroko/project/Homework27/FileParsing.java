8d03d06ba9cdb4d3b5a9543b4286736d572be8e0
maddojka <yuriy.soroko@gmail.com>
2024-04-16 21:33:08 +0300

    Homework 27 - task 01

---
+++ src/com/soroko/project/Homework27/FileParsing.java
@@ -0,0 +1,58 @@
+package com.soroko.project.Homework27;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileParsing implements Runnable {
+    private String name;
+    private File file;
+    private Thread t;
+    private String filePath;
+
+
+    public FileParsing(String name, String filePath) {
+        this.name = name;
+        this.filePath = filePath;
+        t = new Thread(this, name);
+        file = new File(filePath);
+    }
+
+    public Thread getT() {
+        return t;
+    }
+
+    public void parseFile() {
+        int counter = 0;
+        boolean dataExists = false;
+        System.out.println("Результат чтения файла: " +  this.filePath);
+        System.out.println("События с приоритетом 7 и выше:");
+        try (Scanner scan = new Scanner(file);) {
+            while (scan.hasNext()) {
+                String nextLine = scan.nextLine();
+                String[] data = nextLine.split("::");
+                for (int i = 1; i < data.length; i += 2) {
+                    int priority = Integer.parseInt(data[i]);
+                    if (priority >= 7) {
+                        System.out.println("Дата и время: " + data[0] + ", приоритет "
+                                + priority + ", событие: " + data[2]);
+                        counter++;
+                        dataExists = true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("При чтении файла возникла ошибка");
+        }
+        if (!dataExists) {
+            System.out.println("По данному критерию событий не найдено");
+        }
+        System.out.println("Количество событий с приоритетом 7 и выше: " + (counter));
+    }
+
+    @Override
+    public void run() {
+        parseFile();
+    }
+}
---
+++ src/com/soroko/project/Homework27/Task1.java
@@ -0,0 +1,32 @@
+package com.soroko.project.Homework27;
+
+public class Task1 {
+    public static final String FILE_PATH_1 =
+            "C:\\Users\\yuriy\\IdeaProjects\\lessons\\src\\com\\soroko\\project\\Homework27\\file1.txt";
+    public static final String FILE_PATH_2 =
+            "C:\\Users\\yuriy\\IdeaProjects\\lessons\\src\\com\\soroko\\project\\Homework27\\file2.txt";
+    public static final String FILE_PATH_3 =
+            "C:\\Users\\yuriy\\IdeaProjects\\lessons\\src\\com\\soroko\\project\\Homework27\\file3.txt";
+    public static final String FILE_PATH_4 =
+            "C:\\Users\\yuriy\\IdeaProjects\\lessons\\src\\com\\soroko\\project\\Homework27\\file4.txt";
+
+    public static void main(String[] args) throws InterruptedException {
+        FileParsing fileParsing1 = createAndStart("firstThread", FILE_PATH_1);
+        fileParsing1.getT().join();
+        System.out.println();
+        FileParsing fileParsing2 = createAndStart("secondThread", FILE_PATH_2);
+        fileParsing2.getT().join();
+        System.out.println();
+        FileParsing fileParsing3 = createAndStart("thirdThread", FILE_PATH_3);
+        fileParsing3.getT().join();
+        System.out.println();
+        FileParsing fileParsing4 = createAndStart("fourthThread", FILE_PATH_4);
+        fileParsing4.getT().join();
+    }
+
+    public static FileParsing createAndStart(String name, String filePath) {
+        FileParsing fileParsing = new FileParsing(name, filePath);
+        fileParsing.getT().start();
+        return fileParsing;
+    }
+}
